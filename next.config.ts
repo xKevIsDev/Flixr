16901a7dbe90bcf5a4db5d9aceab398a8da7072d
KevIsDev <zennerd404@gmail.com>
2024-11-20 03:39:48 +0000

    fix: image domain for tmdb

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['image.tmdb.org', 'upload.wikimedia.org'],
+    domains: [
+      'image.tmdb.org', 
+      'upload.wikimedia.org',
+      'upload.wikimedia.org',
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/wikipedia/commons/**',
+      }
+    ]
   },
 };
 
