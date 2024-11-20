5b0e43ff10eeb1ad95c9a617c54f638bb6896f4f
KevIsDev <zennerd404@gmail.com>
2024-11-20 03:43:27 +0000

    fix: images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    unoptimized: true,
     domains: [
       'image.tmdb.org', 
       'upload.wikimedia.org',
